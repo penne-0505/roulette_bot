67ad79b90a76e9e5ac374f11ee3a6033c16046c7
penne <penne0505pp@gmail.com>
2025-01-26 10:52:58 +0900

    feat: ログ装飾用のユーティリティを追加

---
+++ roulette_ds_bot/src/utils.py
@@ -0,0 +1,48 @@
+from colorama import Fore, Style
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+# logging constants
+INFO = f"{Fore.BLUE}[INFO]{Style.RESET_ALL}: "
+ERROR = f"{Fore.RED}[ERROR]{Style.RESET_ALL}: "
+WARN = f"{Fore.YELLOW}[WARNING]{Style.RESET_ALL}: "
+DEBUG = f"{Fore.MAGENTA}[DEBUG]{Style.RESET_ALL}: "
+SUCCESS = f"{Fore.GREEN}[SUCCESS]{Style.RESET_ALL}: "
+FORMAT = str(f"{Style.BRIGHT}%(asctime)s{Style.RESET_ALL} - %(message)s")
+DATEFORMAT = str("%Y-%m-%d %H:%M:%S")
+
+
+def blue(text: str) -> str:
+    return f"{Fore.BLUE}{text}{Style.RESET_ALL}"
+
+
+def red(text: str) -> str:
+    return f"{Fore.RED}{text}{Style.RESET_ALL}"
+
+
+def yellow(text: str) -> str:
+    return f"{Fore.YELLOW}{text}{Style.RESET_ALL}"
+
+
+def magenta(text: str) -> str:
+    return f"{Fore.MAGENTA}{text}{Style.RESET_ALL}"
+
+
+def green(text: str) -> str:
+    return f"{Fore.GREEN}{text}{Style.RESET_ALL}"
+
+
+def cyan(text: str) -> str:
+    return f"{Fore.CYAN}{text}{Style.RESET_ALL}"
+
+
+def bold(text: str) -> str:
+    return f"{Style.BRIGHT}{text}{Style.RESET_ALL}"
